997d4b6f7b5200f128edbc0ad2b4d0ee003a6de3
VINICIUS ALVES PRIULI <2971392321023@fatecmatao.local>
2026-02-11 08:49:11 -0300

    Criacao da classe Servico dentro de Domain

---
+++ lab_mobile_kickoff/domain/Servico.dart
@@ -0,0 +1,12 @@
+class Servico {
+  final int id;
+  final String titulo;
+  final String descricao;
+
+  // Construtor: os nomes entre chaves {} são parâmetros nomeados
+  Servico({
+    required this.id,
+    required this.titulo,
+    required this.descricao,
+  });
+}
